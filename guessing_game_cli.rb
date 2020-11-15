0d6488752648fe31f72b558be26f272e4c1c91ee
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 19:47:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,14 @@
 # Code your solution here!
 
 
-def run_guessing_game
 
+
+
+
+
+
+def run_guessing_game
+  random_number = rand(1..6)
+  puts "Please guess a number between 1 and 6"
+  input = gets.strip
 end
