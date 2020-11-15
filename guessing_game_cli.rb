5d0e9dbf9d92665e8289a3c9e965ade135761e93
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 20:01:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  else input != random_number
+  else
     puts "Sorry! The computer guessed #{random_number}."
   end
 end
