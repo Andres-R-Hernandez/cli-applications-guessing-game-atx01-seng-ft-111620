71f065cc72de6b11925554302335cb03533efa9b
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 19:53:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@
 
 def run_guessing_game
   random_number = rand(6) + 1
-  puts "Please guess a number between 1 and 6"
+  puts "Please guess a number between 1 and 6."
   input = gets.strip
   if input == random_number
     puts "You guessed the correct number!"
